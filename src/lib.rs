67f824d3020a0ae992cc2bd8748df1b6e471bedf
George Makdesi <george.makdesi@icloud.com>
2026-04-20 04:25:43 +0200

    Make memory-error public.

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,7 @@ mod transform;
 
 // Public exports
 pub use crate::cast::{CastError, TryCast};
+pub use crate::core::mem::error::{MemoryError, OnError};
 pub use crate::tensor::Tensor;
+
 pub use meta::tensor;
